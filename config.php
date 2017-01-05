6d36e6c2e00cebc8bf4673a30af0074337b729af
1218666840 <shekmao@gmail.com>
2017-01-05 11:35:25 +0800

    安装

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mshop/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/mshop/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/mshop/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/mshop/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/mshop/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/mshop/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/mshop/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/mshop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/mshop/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/mshop/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/mshop/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/mshop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
